1234a17f6e5bac1dd8ba894b29edb416dd0a8b72
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-03-10 16:57:08 +0100

    parent schedule

---
+++ app/src/main/java/com/example/bezpiecznedziecko/parent/schedules/parentSchedule.java
@@ -0,0 +1,13 @@
+package com.example.bezpiecznedziecko.parent.schedules;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.bezpiecznedziecko.R;
+
+public class parentSchedule extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parent_schedule);
+    }
+}
