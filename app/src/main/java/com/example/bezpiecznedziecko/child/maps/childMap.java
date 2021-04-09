660c984e61e67e8ff166208f9ba044b08603ef1c
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-04-09 22:50:20 +0200

    odświeżanie pozycji dziecka przyciskiem

--- app/src/main/java/com/example/bezpiecznedziecko/child/maps/childMap.java
+++ app/src/main/java/com/example/bezpiecznedziecko/child/maps/childMap.java
@@ -3,11 +3,15 @@ package com.example.bezpiecznedziecko.child.maps;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.bezpiecznedziecko.R;
+import com.example.bezpiecznedziecko.child.main.childMain;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class childMap extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button tmp_btn_refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,25 +34,29 @@ public class childMap extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        tmp_btn_refresh = (Button)findViewById(R.id.tmp_btn_refresh);
+        tmp_btn_refresh.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                onMapReady(mMap);
+            }
+        });
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
+        mMap.clear();
 
         SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.shared_preferences), Context.MODE_PRIVATE);
-        Float longitude = sharedPref.getFloat(getString(R.string.shared_preferences_longitude), (float) 0.0);
-        Float latitude = sharedPref.getFloat(getString(R.string.shared_preferences_latitude), (float) 0.0);
+        float longitude = sharedPref.getFloat(getString(R.string.shared_preferences_longitude), (float) 0.0);
+        float latitude = sharedPref.getFloat(getString(R.string.shared_preferences_latitude), (float) 0.0);
 
         LatLng latlng = new LatLng(latitude, longitude);
 
